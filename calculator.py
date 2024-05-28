2f4f78b7f7018a3f6e6003ab3fa0268202429851
kyumin249 <kyumin1429@naver.com>
2024-05-28 19:53:37 +0900

    Create calculator.py
    사칙연산 프로그램 (컴퓨팅 사고와 코딩 기초 13주차 수행)

---
+++ calculator.py
@@ -0,0 +1,34 @@
+import random
+print("사칙연산 프로그램")
+op = input("덧셈 1,뺄셈 2, 곱셈 3, 나눗셈 4(종료시 enter):")
+
+while True:
+    a = random.randint(1,10)
+    b = random.tandint(1, 10)
+
+    if op == "1":
+        result1 = int(input("a+b="))
+        if result1 == a+b:
+            print("정답입니다.")
+        else:
+            print("틀렸습니다.")
+    elif op == "2":
+        result2 = int(input("a-b="))
+        if result2 == a-b:
+            print("정답입니다.")
+        else:
+            print("틀렸습니다.")
+    elif op == "3":
+        result2 = int(input("a*b="))
+        if result2 == a*b:
+            print("정답입니다.")
+        else:
+            print("틀렸습니다.")
+    elif op == "4":
+        result2 = int(input("a/b="))
+        if result2 == a/b:
+            print("정답입니다.")
+        else:
+            print("틀렸습니다.")
+    else:
+        print("error")
