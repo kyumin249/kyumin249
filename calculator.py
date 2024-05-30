c505e38eb0ed9fa3e7f6ee4f9a6a84693265c57d
kyumin249 <kyumin1429@naver.com>
2024-05-30 20:09:43 +0900

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,28 +4,28 @@ op = input("덧셈 1,뺄셈 2, 곱셈 3, 나눗셈 4(종료시 enter):")
 
 while True:
     a = random.randint(1,10)
-    b = random.tandint(1, 10)
+    b = random.randint(1, 10)
 
     if op == "1":
-        result1 = int(input("a+b="))
+        result1 = int(input(f"{a}+{b}="))
         if result1 == a+b:
             print("정답입니다.")
         else:
             print("틀렸습니다.")
     elif op == "2":
-        result2 = int(input("a-b="))
+        result2 = int(input("f{a}-{b}="))
         if result2 == a-b:
             print("정답입니다.")
         else:
             print("틀렸습니다.")
     elif op == "3":
-        result2 = int(input("a*b="))
+        result2 = int(input("{a}*{b}="))
         if result2 == a*b:
             print("정답입니다.")
         else:
             print("틀렸습니다.")
     elif op == "4":
-        result2 = int(input("a/b="))
+        result2 = int(input(f"{a}/{b}="))
         if result2 == a/b:
             print("정답입니다.")
         else:
